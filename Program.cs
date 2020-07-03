c55df029bd0ec09b757a2d73bd85a3945d53c729
Pritam Basu <34293684+pritambasu@users.noreply.github.com>
2020-07-03 12:14:30 +0530

    area check

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        public static void area(float a)
+        {
+            double area = 3.14 * a * a;
+            Console.WriteLine("{0}",area);
+        }
+        public static void area(int l,int b)
+        {
+            double area = l*b;
+            Console.WriteLine(area);
+        }
+        public static void area(float h, float b)
+        {
+            double area = h * b/2;
+            Console.WriteLine(area);
+        }
+        static void Main(string[] args)
+        {
+            area(3);
+            area(4,5);
+            area(4f,4f);
+
+        }
+    }
+
+}
